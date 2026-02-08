668d507224a3165e0e054ab845b5b55a49c563cb
ARIIK-ANTHONY <a.ariik@alustudent.com>
2026-02-08 12:56:43 +0200

    4-line_up.py

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def add_arrays(arr1, arr2):
+    """
+    Add two arrays element-wise
+    
+    Args:
+        arr1: First array
+        arr2: Second array
+    
+    Returns:
+        New list with element-wise sum, or None if shapes don't match
+    """
+    if len(arr1) != len(arr2):
+        return None
+    
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
