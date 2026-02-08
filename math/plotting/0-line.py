3b3fc58883dc9b9bcbb0f7802fba60d0246bfdc3
root <root@DESKTOP-BPHIKKF.localdomain>
2026-02-08 11:59:06 +0200

    0-line.py

---
+++ math/plotting/0-line.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.arange(0, 11) ** 3
+
+# your code here
+x = np.arange(0, 11)
+plt.plot(x, y, 'r-')
+plt.xlim(0, 10)
+plt.show()
