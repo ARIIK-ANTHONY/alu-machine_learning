23974f8a735d597301157a65b5201f326e3797ef
ARIIK-ANTHONY <a.ariik@alustudent.com>
2026-02-08 13:11:17 +0200

    11-the_western_exchange.py

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for numpy array transposition."""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """Transpose a numpy matrix.
+
+    Args:
+        matrix: The numpy array to transpose
+
+    Returns:
+        New transposed numpy array
+    """
+    return matrix.T
