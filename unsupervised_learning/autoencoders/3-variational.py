70a90a1b560ba603fddd83b7156f27ebc8025ebb
ARIIK-ANTHONY <a.ariik@alustudent.com>
2026-05-22 22:09:20 +0200

    3-variational.py

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -22,8 +22,10 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         x = keras.layers.Dense(nodes, activation='relu')(x)
 
     # Create two separate Dense layers for mean and log variance
-    z_mean = keras.layers.Dense(latent_dims, activation='linear')(x)
-    z_log_var = keras.layers.Dense(latent_dims, activation='linear')(x)
+    z_mean = keras.layers.Dense(
+        latent_dims, activation='linear')(x)
+    z_log_var = keras.layers.Dense(
+        latent_dims, activation='linear')(x)
 
     def sampling(args):
         """Sampling similar points in latent space"""
@@ -33,7 +35,8 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         epsilon = keras.backend.random_normal(shape=(batch, dim))
         return z_mean + keras.backend.exp(z_log_var / 2) * epsilon
 
-    z = keras.layers.Lambda(sampling, output_shape=(latent_dims,))([z_mean, z_log_var])
+    z = keras.layers.Lambda(
+        sampling, output_shape=(latent_dims,))([z_mean, z_log_var])
     # Encoder returns only the sampled z
     encoder = keras.Model(encoder_input, z)
 
@@ -42,7 +45,8 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     x = decoder_input
     for nodes in reversed(hidden_layers):
         x = keras.layers.Dense(nodes, activation='relu')(x)
-    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder_output = keras.layers.Dense(
+        input_dims, activation='sigmoid')(x)
     decoder = keras.Model(decoder_input, decoder_output)
 
     # Autoencoder
@@ -51,23 +55,23 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     reconstructed = decoder(encoded)
     auto = keras.Model(auto_input, reconstructed)
 
-    # We need to access z_mean and z_log_var for the loss function
-    # Recreate them inside the autoencoder for the loss
-    x_for_loss = auto_input
+    # Recreate mean and log var for loss function
+    x_loss = auto_input
     for nodes in hidden_layers:
-        x_for_loss = keras.layers.Dense(nodes, activation='relu')(x_for_loss)
-    z_mean_loss = keras.layers.Dense(latent_dims, activation='linear')(x_for_loss)
-    z_log_var_loss = keras.layers.Dense(latent_dims, activation='linear')(x_for_loss)
+        x_loss = keras.layers.Dense(nodes, activation='relu')(x_loss)
+    z_mean_loss = keras.layers.Dense(
+        latent_dims, activation='linear')(x_loss)
+    z_log_var_loss = keras.layers.Dense(
+        latent_dims, activation='linear')(x_loss)
 
     def vae_loss(x, x_decoder_mean):
-        """VAE loss function combining reconstruction loss and KL divergence"""
-        reconstruction_loss = keras.backend.binary_crossentropy(x, x_decoder_mean)
-        reconstruction_loss = keras.backend.sum(reconstruction_loss, axis=-1)
+        """VAE loss combining reconstruction loss and KL divergence"""
+        rec_loss = keras.backend.binary_crossentropy(x, x_decoder_mean)
+        rec_loss = keras.backend.sum(rec_loss, axis=-1)
         kl_loss = -0.5 * keras.backend.sum(
-            1 + z_log_var_loss - keras.backend.square(z_mean_loss) - keras.backend.exp(z_log_var_loss),
-            axis=-1
-        )
-        return keras.backend.mean(reconstruction_loss + kl_loss)
+            1 + z_log_var_loss - keras.backend.square(z_mean_loss) -
+            keras.backend.exp(z_log_var_loss), axis=-1)
+        return keras.backend.mean(rec_loss + kl_loss)
 
     auto.compile(loss=vae_loss, optimizer='adam')
     return encoder, decoder, auto
