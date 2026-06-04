70d15b22126e1f7fe285de807a4aee691a9c4364
ARIIK-ANTHONY <a.ariik@alustudent.com>
2026-06-04 02:19:47 +0200

    8-RMSProp.py

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -9,10 +9,10 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
         using the RMSProp optimization algorithm
 
     Args:
-        loss: is the loss of the network
-        alpha: is the learning rate
-        beta2: is the RMSProp weight
-        epsilon: is a small number to avoid division by zero
+        loss: the loss of the network
+        alpha: the learning rate
+        beta2: the RMSProp weight
+        epsilon: a small number to avoid division by zero
 
     Returns:
         the RMSProp optimization operation
